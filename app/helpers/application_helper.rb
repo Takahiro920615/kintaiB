c11bab75a0f0796f966824b61b9bb033dfcfa846
EC2 Default User <test.@email.com>
2022-08-30 13:19:34 +0000

    head作成完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+# full_titleヘルパーメソッド　page_nameが空白かどうか検証している
+  def full_title(page_name = "")
+    base_title = "KintaiApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'kintaipages/top'
+  root 'kintaipages#top'
   
   resources :kintaipages
 
