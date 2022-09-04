d1dbc8444aa7c07bb9fbbd3886682a32707f992e
EC2 Default User <test.@email.com>
2022-09-04 21:23:39 +0000

    エラーのビュー完成

--- app/controllers/kintaipages_controller.rb
+++ app/controllers/kintaipages_controller.rb
@@ -2,7 +2,5 @@ class KintaipagesController < ApplicationController
   def top
   end
   
-  def edit
-    @user = User.find(params[:id])
-  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,18 @@ class UsersController < ApplicationController
   before_action :set_user, only:  %i(new create edit)
   
   def new
-    
+    @user = User.new(user_params)
   end
+    
+
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render.new
+    end
     
   end
   
@@ -14,11 +22,23 @@ class UsersController < ApplicationController
   
   end
   
+  def index
+  end
+  
+  def show
+  end
+  
   
   private
   
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 
 end
