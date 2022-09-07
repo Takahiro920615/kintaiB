d28e843493b37c054e079fed28ec020e679dfd8f
EC2 Default User <test.@email.com>
2022-09-07 14:31:00 +0000

    永続セッション完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase}
   #selfはメソッドを呼び出している時点のユーザーオブジェクトを指している。
   
@@ -12,4 +13,29 @@ class User < ApplicationRecord
             
   has_secure_password
   validates :password, presence: true, length: { minimum: 6}
+  
+  def User.digest(string)
+    cost =
+    if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+ 
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
