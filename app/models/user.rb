bed8621f509fc306a090fbcb94b6ebca72812028
EC2 Default User <test.@email.com>
2022-10-04 12:49:35 +0000

    UIテスト、追加機能確認完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
  validates :department, length: {in:2..30}, allow_blank: true
  validates :basic_time, presence:true
  validates :work_time, presence:true
- validates :worked_on ,presence:true
+
             
   has_secure_password
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
